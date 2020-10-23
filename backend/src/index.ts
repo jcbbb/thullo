0fcb6857022434b1e7f0802eb5d7c1891f2c6515
jc <avaz28082000@gmail.com>
2020-10-23 20:46:12 +0500

    Router init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import config from './config';
+import router from './routes';
 import { db } from './services/db';
 
 const app = express();
 
 app.use(express.json());
+app.use('/', router);
 
 db.connect().then(() => {
   app.listen(config.port, () => console.log(`Server started on port ${config.port}`));
