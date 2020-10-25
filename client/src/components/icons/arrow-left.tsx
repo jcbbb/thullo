c8f5d5ecc5aa2c377bfa57186321c010b409a9ab
jc <avaz28082000@gmail.com>
2020-10-25 23:45:38 +0500

    New icons

---
+++ client/src/components/icons/arrow-left.tsx
@@ -0,0 +1,5 @@
+import withIcon from '../icon';
+
+const arrowLeft = `<path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>`;
+
+export default withIcon(arrowLeft);
---
+++ client/src/components/icons/chevron-down.tsx
@@ -0,0 +1,5 @@
+import withIcon from '../icon';
+
+const chevronDown = `<path d="M0 0h24v24H0z" fill="none"/><path d="M7 10l5 5 5-5z"/>`;
+
+export default withIcon(chevronDown);
