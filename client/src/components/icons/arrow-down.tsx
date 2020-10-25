8ee750c0e6f10a404834a995270e810acfb4a08a
jc <avaz28082000@gmail.com>
2020-10-25 23:54:27 +0500

    Moved icons

--- client/src/components/icons/arrow-down.tsx
+++
@@ -1,5 +0,0 @@
-import withIcon from "../icon";
-
-const arrowDown = `<path d="M0 0h24v24H0z" fill="none"/><path d="M7 10l5 5 5-5z"/>`;
-
-export default withIcon(arrowDown);
--- client/src/components/icons/arrow-up.tsx
+++
@@ -1,5 +0,0 @@
-import withIcon from "../icon";
-
-const arrowUp = `<path d="M0 0h24v24H0z" fill="none"/><path d="M7 14l5-5 5 5z"/>`;
-
-export default withIcon(arrowUp);
