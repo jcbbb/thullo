7184cbdb2d349b958a6b03f4ca802125f610d0b1
jc <avaz28082000@gmail.com>
2020-10-26 00:04:16 +0500

    Added formiz

--- client/src/components/formInput/formInput.tsx
+++ client/src/components/formInput/formInput.tsx
@@ -1,31 +1,29 @@
 import * as React from 'react';
 import inputStyles from '../../styles/input.module.scss';
-import useFormInput from '../../hooks/useFormInput';
-import { IValidation } from '../../hooks/useForm';
+import cn from 'classnames';
+import { useField } from '@formiz/core';
 
-export interface IFormInput {
-  type?: string;
-  name: string;
-  validations?: IValidation[];
-  placeholder?: string;
-}
-const FormInput = (props: IFormInput) => {
-  const { value, setValue, isValid, isPristine, errorMessage } = useFormInput(props);
-  const { type, placeholder, name } = props;
+const FormInput = React.forwardRef((props: any, ref: any) => {
+  const { setValue, value, isValid, isPristine, errorMessage, isSubmitted } = useField(props);
+  const { name, placeholder, type, maxLength, autoComplete } = props;
 
+  const showError = !isValid && (!isPristine || isSubmitted);
   return (
     <>
       <input
-        className={inputStyles.formInput}
+        className={cn(inputStyles.formInput, { [inputStyles.errorInput]: showError })}
         name={name}
         value={value ?? ''}
         type={type || 'text'}
         placeholder={placeholder || ''}
         onChange={({ target }) => setValue(target.value)}
+        maxLength={maxLength}
+        autoComplete={autoComplete}
+        ref={ref}
       />
-      {!isValid && !isPristine && <span className={inputStyles.errorText}>{errorMessage}</span>}
+      {showError && <span className={inputStyles.errorText}>{errorMessage}</span>}
     </>
   );
-};
+});
 
 export default FormInput;
