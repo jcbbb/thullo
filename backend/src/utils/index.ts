15088a111428efb59a3f2a516d7c73e626d40c49
jc <avaz28082000@gmail.com>
2020-10-26 23:34:45 +0500

    Move setting cookies into it's own function

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -3,7 +3,12 @@ import jwt from 'jsonwebtoken';
 import { IUser } from '../models/user';
 import config from '../config';
 
-export const generateToken = (res: Response, user: IUser) => {
+interface Token {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export const generateToken = (user: IUser): Token => {
   const { _id, email, role } = user;
   const accessToken = jwt.sign({ _id, email, role }, config.access_token_secret, {
     expiresIn: config.access_token_expiration,
@@ -11,6 +16,12 @@ export const generateToken = (res: Response, user: IUser) => {
   const refreshToken = jwt.sign({}, config.refresh_token_secret, {
     expiresIn: config.refresh_token_expiration,
   });
+
+  return { accessToken, refreshToken };
+};
+
+export const setCookies = (res: Response, token: Token) => {
+  const { accessToken, refreshToken } = token;
 
   res.cookie('access_token', accessToken, {
     expires: new Date(Date.now() + config.access_token_expiration),
@@ -26,5 +37,7 @@ export const generateToken = (res: Response, user: IUser) => {
     httpOnly: true,
   });
 };
+
+export const randomNumber = () => Math.floor(100000 + Math.random() * 900000);
 
 export const verifyToken = (res: Response, req: Request, next: NextFunction) => {};
