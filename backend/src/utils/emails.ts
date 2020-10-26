97696dff48675b5a1bd202a44a04b4b6811c6f18
jc <avaz28082000@gmail.com>
2020-10-26 23:28:01 +0500

    Email utils

---
+++ backend/src/utils/emails.ts
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+import hbs from 'handlebars';
+
+type Templates = typeof templates;
+export type TemplateKey = keyof Templates;
+
+export const buildTemplateRender = <T>(template: string) => {
+  const file = path.join(__dirname, '../email-templates', `${template}.hbs`);
+  const text = fs.readFileSync(file, 'utf-8');
+  return hbs.compile<T>(text);
+};
+
+export const templates = {
+  'auth-code': {
+    render: buildTemplateRender<{
+      authCode: string;
+    }>('auth-code'),
+    subject: (subject: string = `Your auth code`) => subject,
+  },
+};
+
