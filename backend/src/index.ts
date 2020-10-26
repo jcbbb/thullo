6916651369f0b6a764655b84b8df49571f0c3ef8
jc <avaz28082000@gmail.com>
2020-10-26 23:36:58 +0500

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import config from './config';
 import router from './routes';
+import cors from 'cors';
 import { db } from './services/db';
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }),
+);
 app.use('/', router);
 
 db.connect().then(() => {
