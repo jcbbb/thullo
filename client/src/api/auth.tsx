1c54c4a0b55289669794431762d63a4fa6b6eb92
jc <avaz28082000@gmail.com>
2020-10-27 22:50:28 +0500

    Added new api methods

--- client/src/api/auth.tsx
+++ client/src/api/auth.tsx
@@ -2,13 +2,30 @@ import { request } from '../utils';
 
 export interface IAuthEndpoints {
   checkEmail: (email: string) => Promise<Response | undefined>;
+  createTempUser: (email: string) => Promise<Response | undefined>;
+  verifyAuthCode: (email: string, authCode: string) => Promise<Response | undefined>;
 }
 
 export const createAuthEndoints = (resourceUrl: string): IAuthEndpoints => {
   const endpoints: IAuthEndpoints = {
+    createTempUser: async (email: string) =>
+      await request(`${resourceUrl}/temp-user`, {
+        body: {
+          email,
+        },
+      }),
     checkEmail: async (email: string) =>
       await request(`${resourceUrl}/check-email`, {
-        body: email,
+        body: {
+          email,
+        },
+      }),
+    verifyAuthCode: async (email: string, authCode: string) =>
+      await request(`${resourceUrl}/verify`, {
+        body: {
+          email,
+          authCode,
+        },
       }),
   };
 
