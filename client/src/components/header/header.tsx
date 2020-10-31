d9b5f1e150d7ee2dd21f1f6b84dd703d14b7217f
jc <avaz28082000@gmail.com>
2020-10-31 16:36:48 +0500

    ProfilePic component ready

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import ProfilePic from '../profilePic/profilePic'
 import Logo from '../icons/thullo';
 import ChevronUp from '../icons/chevron-up';
 import ChevronDown from '../icons/chevron-down';
@@ -7,67 +8,67 @@ import Application from '../icons/application';
 import styles from './header.module.scss';
 
 const Header = () => {
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState('');
-  return (
-    <div className={styles.header}>
-      <div className={styles.headerLeft}>
-        <Link to="/">
-          <Logo viewBox="0 0 98 29" size={{ width: 96, height: 54 }} />
-        </Link>
-        <div className={styles.headerLeftInner}>
-          <div className={styles.boardName}>
-            <h2 className={styles.boardNameText}>Devchallenges Board</h2>
-          </div>
-          <span className={styles.seperator}></span>
-          <Link className={styles.boards} to="/">
-            <Application size={{ width: 14, height: 14 }} color="var(--gray-3)" />
-            <span className={styles.boardsText}>All board</span>
-          </Link>
-        </div>
-      </div>
-      <div className={styles.headerRight}>
-        <form className={styles.form}>
-          <input
-            className={styles.input}
-            name="search"
-            type="text"
-            placeholder="Keyword"
-            value={value}
-            onChange={(ev) => setValue(ev.target.value)}
-          />
-          <button className={styles.formBtn} disabled={!value}>
-            Search
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState('');
+    return (
+        <div className={styles.header}>
+            <div className={styles.headerLeft}>
+                <Link to="/">
+                    <Logo viewBox="0 0 98 29" size={{width: 96, height: 54}} />
+                </Link>
+                <div className={styles.headerLeftInner}>
+                    <div className={styles.boardName}>
+                        <h2 className={styles.boardNameText}>Devchallenges Board</h2>
+                    </div>
+                    <span className={styles.seperator}></span>
+                    <Link className={styles.boards} to="/">
+                        <Application size={{width: 14, height: 14}} color="var(--gray-3)" />
+                        <span className={styles.boardsText}>All board</span>
+                    </Link>
+                </div>
+            </div>
+            <div className={styles.headerRight}>
+                <form className={styles.form}>
+                    <input
+                        className={styles.input}
+                        name="search"
+                        type="text"
+                        placeholder="Keyword"
+                        value={value}
+                        onChange={(ev) => setValue(ev.target.value)}
+                    />
+                    <button className={styles.formBtn} disabled={!value}>
+                        Search
           </button>
-        </form>
-        <div className={styles.profile}>
-          <div className={styles.profilePic}></div>
-          <details className={styles.dropdown}>
-            <summary className={styles.summary} onClick={() => setOpen((open) => !open)}>
-              <span className={styles.summaryText}>Xanthe Neal</span>
-              {!open ? (
-                <ChevronUp color="var(--heading-text)" />
-              ) : (
-                <ChevronDown color="var(--heading-text)" />
-              )}
-            </summary>
-            <ul className={styles.links}>
-              <li className={styles.linkItem}>
-                <Link to="/settings" className={styles.link}>
-                  Settings
+                </form>
+                <div className={styles.profile}>
+                    <ProfilePic />
+                    <details className={styles.dropdown}>
+                        <summary className={styles.summary} onClick={() => setOpen((open) => !open)}>
+                            <span className={styles.summaryText}>Xanthe Neal</span>
+                            {!open ? (
+                                <ChevronUp color="var(--heading-text)" />
+                            ) : (
+                                    <ChevronDown color="var(--heading-text)" />
+                                )}
+                        </summary>
+                        <ul className={styles.links}>
+                            <li className={styles.linkItem}>
+                                <Link to="/settings" className={styles.link}>
+                                    Settings
                 </Link>
-              </li>
-              <li className={styles.linkItem}>
-                <Link to="/logout" className={styles.link}>
-                  Log out
+                            </li>
+                            <li className={styles.linkItem}>
+                                <Link to="/logout" className={styles.link}>
+                                    Log out
                 </Link>
-              </li>
-            </ul>
-          </details>
+                            </li>
+                        </ul>
+                    </details>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Header;
--- client/src/components/profilePic/profilePic.tsx
+++ client/src/components/profilePic/profilePic.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react';
 import styles from './profilePic.module.scss';
 
-const ProfilePic = (src: string) => {
-  return (
-    <div className={styles.profileContainer} style={{ backgroundImage: `url(${src})` }}>
-      <img src={src} className={styles.profilePic} alt="default" />
-    </div>
-  );
+type IProps = {
+    src?: string;
+    alt?: string;
+}
+
+const ProfilePic = ({src, alt}: IProps) => {
+    return (
+        <div className={styles.profileContainer}>
+            {src ? (
+                <img src={src} className={styles.profilePic} alt={alt} />
+
+            ) : (
+
+                    <span className={styles.initials}>XN</span>
+                )}
+        </div>
+    );
 };
 
 export default ProfilePic;
