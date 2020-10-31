83e9c87212af3fbcfb5216712a73df0240ade07b
jc <avaz28082000@gmail.com>
2020-10-31 19:16:29 +0500

    Imported boardStatus to see

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,14 @@ import Header from './components/header/header';
 import Login from './components/login/login';
 import Signup from './components/signup/signup';
 import Center from './components/center/center';
+import BoardStatus from './components/boardStatus/boardStatus';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <BoardStatus />
       <Center>
         <Switch>
           <Route path="/signup">
