548edbc4f8a7d025ff1ce1611a63887b4deae4e1
jc <avaz28082000@gmail.com>
2020-11-01 20:18:30 +0500

    Type correction; format properly

--- client/src/components/profilePic/profilePic.tsx
+++ client/src/components/profilePic/profilePic.tsx
@@ -2,22 +2,24 @@ import * as React from 'react';
 import styles from './profilePic.module.scss';
 
 type IProps = {
-    src?: string;
-    alt?: string;
-}
+  src?: string;
+  alt?: string;
+  size?: {
+    width: string | number;
+    height: string | number;
+  };
+};
 
-const ProfilePic = ({src, alt}: IProps) => {
-    return (
-        <div className={styles.profileContainer}>
-            {src ? (
-                <img src={src} className={styles.profilePic} alt={alt} />
-
-            ) : (
-
-                    <span className={styles.initials}>XN</span>
-                )}
-        </div>
-    );
+const ProfilePic = ({ src, alt, size }: IProps) => {
+  return (
+    <div className={styles.profileContainer} style={{ ...size }}>
+      {src ? (
+        <img src={src} className={styles.profilePic} alt={alt} />
+      ) : (
+        <span className={styles.initials}>XN</span>
+      )}
+    </div>
+  );
 };
 
 export default ProfilePic;
