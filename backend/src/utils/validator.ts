e0d1445413566c4bdf5c3f371dd3e76215517bc3
jc <avaz28082000@gmail.com>
2020-11-03 22:14:10 +0500

    Implemented simpler validation strategy

--- backend/src/utils/validator.ts
+++ backend/src/utils/validator.ts
@@ -5,7 +5,9 @@ type ErrorParam = string | number;
 type ErrorMessage = string;
 type ErrorObject = Record<ErrorParam, ErrorMessage>;
 
-export const emailValidationRules = () => [
+type RuleNames = 'email' | 'password' | 'authCode';
+
+const emailValidationRules = () => [
   body('email')
     .notEmpty()
     .isString()
@@ -14,19 +16,35 @@ export const emailValidationRules = () => [
     .withMessage('Email should be valid'),
 ];
 
-export const authCodeValidationRules = () => [
+const authCodeValidationRules = () => [
   body('authCode')
     .notEmpty()
     .isString()
     .isLength({ min: 6, max: 6 })
     .withMessage('Auth code must be 6 characters long and not empty'),
-  body('email')
-    .notEmpty()
-    .isString()
-    .normalizeEmail()
-    .isEmail()
-    .withMessage('Email should be valid'),
 ];
+
+const passwordValidationRules = () => [
+  body('password')
+    .notEmpty()
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters long'),
+];
+
+const rules = {
+  email: emailValidationRules,
+  password: passwordValidationRules,
+  authCode: authCodeValidationRules,
+};
+
+export const validationFor = (...ruleNames: RuleNames[]) => {
+  let validations: any = [];
+  ruleNames.forEach((ruleName) => {
+    validations = [...validations, ...rules[ruleName]()];
+  });
+
+  return validations;
+};
 
 export const validate = (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
