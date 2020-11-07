bea8cc43c5f75be28ed825a0616bbcd08ea50c06
jc <avaz28082000@gmail.com>
2020-11-08 00:48:44 +0500

    Import unsplash endpoints

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,10 +1,12 @@
 import API from './base';
 import { createAuthEndoints } from './auth';
+import { createUnsplashEndpoints } from './unsplash';
 
 const apiUrl = process.env.REACT_APP_API_URL as string;
 
 const api = API({
   auth: createAuthEndoints(`${apiUrl}/auth`),
+  unsplash: createUnsplashEndpoints('https://api.unsplash.com'),
 });
 
 export default api;
