31431d9e4deb743c650c0cbbd314c52e2bec3966
jc <avaz28082000@gmail.com>
2020-11-08 00:49:50 +0500

    Fix cookie not setting in browser

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -14,28 +14,31 @@ export const generateToken = (user: IUser): Token => {
     expiresIn: config.access_token_expiration,
   });
   const refreshToken = jwt.sign({}, config.refresh_token_secret, {
-    expiresIn: config.refresh_token_expiration,
+    expiresIn: `${config.refresh_token_expiration}d`,
   });
 
   return { accessToken, refreshToken };
 };
 
 export const setCookies = (res: Response, token: Token) => {
+  const day = 24 * 3600000;
+  const refreshTokenExpiration = parseInt(config.refresh_token_expiration, 10);
+  const accessTokenExpiration = parseInt(config.access_token_expiration, 10);
   const { accessToken, refreshToken } = token;
 
-  res.cookie('access_token', accessToken, {
-    expires: new Date(Date.now() + config.access_token_expiration),
-    secure: true,
-    sameSite: true,
-    httpOnly: true,
-  });
-
-  return res.cookie('refresh_token', refreshToken, {
-    expires: new Date(Date.now() + config.refresh_token_expiration),
-    secure: true,
-    sameSite: true,
-    httpOnly: true,
-  });
+  return res
+    .cookie('refresh_token', refreshToken, {
+      expires: new Date(Date.now() + refreshTokenExpiration * day),
+      secure: true,
+      sameSite: true,
+      httpOnly: true,
+    })
+    .cookie('access_token', accessToken, {
+      expires: new Date(Date.now() + accessTokenExpiration),
+      secure: true,
+      sameSite: true,
+      httpOnly: true,
+    });
 };
 
 export const randomNumber = () => Math.floor(100000 + Math.random() * 900000);
