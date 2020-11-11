84a7dc57b4445f3979c5396c35ba416599b24bde
jc <avaz28082000@gmail.com>
2020-11-11 23:14:59 +0500

    Nothing changed

--- client/src/components/boards/boards.tsx
+++ client/src/components/boards/boards.tsx
@@ -4,19 +4,19 @@ import styles from './boards.module.scss';
 import buttonStyles from '../../styles/button.module.scss';
 
 const Boards = () => {
-  return (
-    <div className={styles.boards}>
-      <div className={styles.header}>
-        <h2 className={styles.heading}>All boards</h2>
-        <div>
-          <button className={buttonStyles.formBtn}>
-            <PlusIcon size={{ width: 18, height: 18 }} />
-            <span className={styles.btnText}>Add</span>
-          </button>
+    return (
+        <div className={styles.boards}>
+            <div className={styles.header}>
+                <h2 className={styles.heading}>All boards</h2>
+                <div>
+                    <button className={buttonStyles.primary}>
+                        <PlusIcon size={{width: 18, height: 18}} />
+                        <span className={styles.btnText}>Add</span>
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Boards;
