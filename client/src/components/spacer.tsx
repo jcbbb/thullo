09538cc3381c8919ff314cf1f8468bea551a3561
jc <avaz28082000@gmail.com>
2020-11-11 23:19:48 +0500

    add default width to 100%

--- client/src/components/spacer.tsx
+++ client/src/components/spacer.tsx
@@ -1,32 +1,35 @@
-import React, { FunctionComponent } from 'react';
+import * as React from 'react';
 
 interface ISpacer {
-  top?: string;
-  left?: string;
-  bottom?: string;
-  right?: string;
+    top?: string;
+    left?: string;
+    bottom?: string;
+    right?: string;
+    width?: string;
 }
 
-const Spacer: FunctionComponent<ISpacer> = ({
-  top = '1em',
-  left = '1em',
-  bottom = '1em',
-  right = '1em',
-  children,
+const Spacer: React.FC<ISpacer> = ({
+    top = '1em',
+    left = '1em',
+    bottom = '1em',
+    right = '1em',
+    width = '100%',
+    children,
 }) => {
-  return (
-    <div
-      style={{
-        width: '100%',
-        marginTop: top,
-        marginLeft: left,
-        marginBottom: bottom,
-        marginRight: right,
-      }}
-    >
-      {children}
-    </div>
-  );
+
+    return (
+        <div
+            style={{
+                width,
+                marginTop: top,
+                marginLeft: left,
+                marginBottom: bottom,
+                marginRight: right,
+            }}
+        >
+            {children}
+        </div>
+    );
 };
 
 export default Spacer;
