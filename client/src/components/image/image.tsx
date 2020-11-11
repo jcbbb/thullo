d577286c1c715617d50afe3d15cc295228ad94ae
jc <avaz28082000@gmail.com>
2020-11-11 23:16:22 +0500

    accept any props

--- client/src/components/image/image.tsx
+++ client/src/components/image/image.tsx
@@ -2,22 +2,23 @@ import * as React from 'react';
 import styles from './image.module.scss';
 
 type IProps = {
-  size?: {
-    width: string | number;
-    height: string | number;
-  };
-  src?: string;
-  alt?: string;
+    size?: {
+        width: string | number;
+        height: string | number;
+    };
+    src?: string;
+    alt?: string;
+    [key: string]: any;
 };
 
-const Image = ({ size = { width: 219, height: 130 }, src, alt, ...rest }: IProps) => {
-  return (
-    <div
-      className={styles.image}
-      style={{ ...size, backgroundImage: `url(${src})` }}
-      {...rest}
-    ></div>
-  );
+const Image = ({size = {width: 219, height: 130}, src, alt, ...rest}: IProps) => {
+    return (
+        <div
+            className={styles.image}
+            style={{...size, backgroundImage: `url(${src})`}}
+            {...rest}
+        ></div>
+    );
 };
 
 export default Image;
