d95faac81d22a9ac4b528647413181d7f61de2cb
jc <avaz28082000@gmail.com>
2020-11-11 23:12:56 +0500

    Probably finished this component;

--- client/src/components/addBoard/addBoard.tsx
+++ client/src/components/addBoard/addBoard.tsx
@@ -1,19 +1,32 @@
 import * as React from 'react'
-import {useEffect} from 'react';
+import {useEffect, useState, useCallback} from 'react';
 import BoardStatus from '../boardStatus/boardStatus'
 import PhotoSearch from '../photoSearch/photoSearch'
 import Spacer from '../spacer'
 import useAsync from '../../hooks/useAsync';
 import useMounted from '../../hooks/useMounted';
 import CloseIcon from '../icons/close'
+import PlusIcon from '../icons/plus';
 import api from '../../api';
 import cn from 'classnames';
 import styles from './addBoard.module.scss'
 import buttonStyles from '../../styles/button.module.scss'
 
+type IInitialValues = {
+    cover_photo_url: string;
+    title: string;
+    status: string;
+}
+
+const initialFormValues = {
+    cover_photo_url: '',
+    title: '',
+    status: 'Public'
+}
+
 const AddBoard = () => {
     const isMounted = useMounted()
-    const [getRandom, {data}] = useAsync(api.unsplash.getRandom);
+    const [getRandom, {data}, reset] = useAsync(api.unsplash.getRandom);
 
     useEffect(() => {
         if (isMounted) {
@@ -21,23 +34,61 @@ const AddBoard = () => {
         }
     }, [getRandom, isMounted])
 
+    const [formValues, setFormValues] = useState(initialFormValues)
+
+    const handleStatus = useCallback((status: string) => {
+        setFormValues((prevState: IInitialValues) => ({
+            ...prevState,
+            status
+        }))
+    }, [setFormValues])
+
+    const handleCover = useCallback((cover_photo_url: string) => {
+        setFormValues((prevState: IInitialValues) => ({...prevState, cover_photo_url}))
+    }, [setFormValues])
+
+    const handleInput = (ev: React.ChangeEvent<HTMLInputElement>) => {
+        const {value} = ev.target;
+        setFormValues((prevState: IInitialValues) => ({
+            ...prevState,
+            title: value
+        }))
+    }
+    const clear = useCallback(() => {
+        setFormValues((prevState) => ({...prevState, cover_photo_url: ''}))
+        reset();
+    }, [reset, setFormValues])
+
     return <div className={styles.container}>
         <div className={styles.cover}>
-            <div className={styles.image} style={{backgroundImage: `url(${data?.urls.small})`}}></div>
-            <button className={cn(buttonStyles.formBtn, styles.btn)}>
-                <CloseIcon size={{width: 18, height: 18}} />
-            </button>
-            <form className={styles.form}>
-                <input className={styles.input} type="text" placeholder="Add board title" />
-                <div className={styles.settings}>
-                    <Spacer left="0.5rem" right="0.5rem" top="1.2rem" bottom="1.2rem">
-                        <BoardStatus />
-                    </Spacer>
-                    <Spacer left="0.5rem" right="0.5rem" top="1.2rem" bottom="1.2rem">
-                        <PhotoSearch />
-                    </Spacer>
-                </div>
-            </form>
+            <div style={formValues.cover_photo_url || data ? {backgroundImage: `url(${formValues.cover_photo_url || data?.urls.small})`} : void 0} className={styles.image}></div>
+            {(formValues.cover_photo_url || data) && (
+                <button className={cn(buttonStyles.primary, styles.btn)} onClick={clear}>
+                    <CloseIcon size={{width: 18, height: 18}} />
+                </button>
+            )}
+            <Spacer top="0.6rem" bottom="0" left="0" right="0">
+                <form className={styles.form}>
+                    <input className={styles.input} value={formValues.title} onChange={handleInput} type="text" placeholder="Add board title" />
+                </form>
+            </Spacer>
+            <div className={styles.settings}>
+                <Spacer left="0.5rem" right="0.5rem" top="1.2rem" bottom="1.2rem">
+                    <BoardStatus status={formValues.status} setStatus={handleStatus} />
+                </Spacer>
+                <Spacer left="0.5rem" right="0.5rem" top="1.2rem" bottom="1.2rem">
+                    <PhotoSearch setCover={handleCover} />
+                </Spacer>
+            </div>
+            <div className={styles.actions}>
+                <Spacer top="0" bottom="0" right="0.8em" width="auto">
+                    <button className={buttonStyles.secondary}>Cancel</button>
+                </Spacer>
+                <button className={buttonStyles.primary} >
+                    <PlusIcon size={{width: 18, height: 18}} />
+                    <span className={styles.btnText}>Create</span>
+                </button>
+            </div>
         </div>
     </div>
 }
