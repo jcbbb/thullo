ea3428fdbda5cc9d53ceac792ffa79928f20d72b
jc <avaz28082000@gmail.com>
2020-11-11 23:15:44 +0500

    Added some checks to show for authenticated users

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,75 +1,79 @@
 import * as React from 'react';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
 import ProfilePic from '../profilePic/profilePic';
 import LogoIcon from '../icons/thullo';
 import ChevronUpIcon from '../icons/chevron-up';
 import ChevronDownIcon from '../icons/chevron-down';
 import ApplicationIcon from '../icons/application';
+import {useAuthStore} from '../../context/authContext'
 import styles from './header.module.scss';
 
 const Header = () => {
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState('');
-  return (
-    <div className={styles.header}>
-      <div className={styles.headerLeft}>
-        <Link to="/">
-          <LogoIcon viewBox="0 0 98 29" size={{ width: 96, height: 54 }} />
-        </Link>
-        <div className={styles.headerLeftInner}>
-          <div className={styles.boardName}>
-            <h2 className={styles.boardNameText}>Devchallenges Board</h2>
-          </div>
-          <span className={styles.seperator}></span>
-          <Link className={styles.boards} to="/">
-            <ApplicationIcon size={{ width: 14, height: 14 }} color="var(--gray-3)" />
-            <span className={styles.boardsText}>All board</span>
-          </Link>
-        </div>
-      </div>
-      <div className={styles.headerRight}>
-        <form className={styles.form}>
-          <input
-            className={styles.input}
-            name="search"
-            type="text"
-            placeholder="Keyword"
-            value={value}
-            onChange={(ev) => setValue(ev.target.value)}
-          />
-          <button className={styles.formBtn} style={{ width: 'fit-content' }} disabled={!value}>
-            Search
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState('');
+    const {isAuthenticated} = useAuthStore();
+    return (
+        <div className={styles.header}>
+            <div className={styles.headerLeft}>
+                <Link to="/">
+                    <LogoIcon viewBox="0 0 98 29" size={{width: 96, height: 54}} />
+                </Link>
+                <div className={styles.headerLeftInner}>
+                    <div className={styles.boardName}>
+                        <h2 className={styles.boardNameText}>Devchallenges Board</h2>
+                    </div>
+                    <span className={styles.seperator}></span>
+                    <Link className={styles.boards} to="/">
+                        <ApplicationIcon size={{width: 14, height: 14}} color="var(--gray-3)" />
+                        <span className={styles.boardsText}>All board</span>
+                    </Link>
+                </div>
+            </div>
+            {isAuthenticated ? (
+                <div className={styles.headerRight}>
+                    <form className={styles.form}>
+                        <input
+                            className={styles.input}
+                            name="search"
+                            type="text"
+                            placeholder="Keyword"
+                            value={value}
+                            onChange={(ev) => setValue(ev.target.value)}
+                        />
+                        <button className={styles.formBtn} style={{width: 'fit-content'}} disabled={!value}>
+                            Search
           </button>
-        </form>
-        <div className={styles.profile}>
-          <ProfilePic />
-          <details className={styles.dropdown}>
-            <summary className={styles.summary} onClick={() => setOpen((open) => !open)}>
-              <span className={styles.summaryText}>Xanthe Neal</span>
-              {!open ? (
-                <ChevronUpIcon color="var(--heading-text)" />
-              ) : (
-                <ChevronDownIcon color="var(--heading-text)" />
-              )}
-            </summary>
-            <ul className={styles.links}>
-              <li className={styles.linkItem}>
-                <Link to="/settings" className={styles.link}>
-                  Settings
+                    </form>
+                    <div className={styles.profile}>
+                        <ProfilePic />
+                        <details className={styles.dropdown}>
+                            <summary className={styles.summary} onClick={() => setOpen((open) => !open)}>
+                                <span className={styles.summaryText}>Xanthe Neal</span>
+                                {!open ? (
+                                    <ChevronUpIcon color="var(--heading-text)" />
+                                ) : (
+                                        <ChevronDownIcon color="var(--heading-text)" />
+                                    )}
+                            </summary>
+                            <ul className={styles.links}>
+                                <li className={styles.linkItem}>
+                                    <Link to="/settings" className={styles.link}>
+                                        Settings
                 </Link>
-              </li>
-              <li className={styles.linkItem}>
-                <Link to="/logout" className={styles.link}>
-                  Log out
+                                </li>
+                                <li className={styles.linkItem}>
+                                    <Link to="/logout" className={styles.link}>
+                                        Log out
                 </Link>
-              </li>
-            </ul>
-          </details>
-        </div>
-      </div>
-    </div>
-  );
+                                </li>
+                            </ul>
+                        </details>
+                    </div>
+                </div>
+
+            ) : null} </div>
+    );
 };
 
 export default Header;
