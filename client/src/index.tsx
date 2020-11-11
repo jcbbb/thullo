1d3f3ea9a238d62933f8e5090fea6112493d21cf
jc <avaz28082000@gmail.com>
2020-11-11 23:21:42 +0500

    Move styles at the bottom

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.scss";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
+import "./styles/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
