f2995299800a801c44ed148ed5c7fcf017a1b2a1
jc <avaz28082000@gmail.com>
2020-11-11 23:13:56 +0500

    Refactor with hooks and fix some styles

--- client/src/components/boardStatus/boardStatus.tsx
+++ client/src/components/boardStatus/boardStatus.tsx
@@ -1,77 +1,68 @@
 import * as React from 'react';
-import { useState, useCallback, useEffect, useRef } from 'react';
+import {useState, useCallback, useRef, useMemo} from 'react';
+import useClickOutside from '../../hooks/useClickOutside';
 import Globe from '../icons/globe';
 import Lock from '../icons/lock';
 import styles from './boardStatus.module.scss';
 
-const BoardStatus = () => {
-  const detailsRef = useRef<HTMLDetailsElement>(null!);
-  const [open, setOpen] = useState(false);
+type IProps = {
+    status: string;
+    setStatus: (status: string) => void;
+}
 
-  const handleEscape = useCallback(
-    (ev) => {
-      if (ev.keyCode === 27 && open) {
-        setOpen(false);
-      }
-    },
-    [open, setOpen]
-  );
+const BoardStatus = ({status, setStatus}: IProps) => {
+    const [open, setOpen] = useState(false);
+    const detailsRef = useRef<HTMLDetailsElement>(null!);
 
-  const handleOutside = useCallback(
-    (ev) => {
-      if (!detailsRef?.current.contains(ev.target as Node)) {
-        setOpen(false);
-      }
-    },
-    [setOpen]
-  );
+    const isPrivate = useMemo(() => status === 'Private', [status])
 
-  const handleClick = useCallback(
-    (ev: React.MouseEvent<HTMLElement>) => {
-      ev.preventDefault();
-      setOpen((open) => !open);
-    },
-    [setOpen]
-  );
+    useClickOutside(detailsRef, () => setOpen(false))
 
-  useEffect(() => {
-    document.addEventListener('keydown', handleEscape);
-    return () => document.removeEventListener('keydown', handleEscape);
-  }, [handleEscape]);
+    const handleClose = (ev: React.MouseEvent<HTMLElement>) => {
+        ev.preventDefault();
+        setOpen((open) => !open);
+    }
 
-  useEffect(() => {
-    document.addEventListener('click', handleOutside);
-    return () => document.removeEventListener('click', handleOutside);
-  }, [handleOutside]);
+    const handleKeyDown = (ev: React.KeyboardEvent<HTMLElement>) => {
+        const {value} = (ev.target as HTMLLIElement).dataset;
+        if (ev.keyCode === 13) {
+            setStatus(value!)
+        }
+    }
 
-  return (
-    <details className={styles.boardStatus} open={open} ref={detailsRef}>
-      <summary className={styles.summary} onClick={handleClick}>
-        <Lock size={{ width: 14, height: 14 }} color="var(--gray-3)" />
-        <span className={styles.summaryText}>Private</span>
-      </summary>
-      <div className={styles.dropdown}>
-        <h4 className={styles.dropdownHeading}>Visibility</h4>
-        <p className={styles.dropdownDetails}>Choose who can view this board</p>
-        <ul className={styles.options}>
-          <li className={styles.option}>
-            <div className={styles.optionHeading}>
-              <Globe size={{ width: 14, height: 14 }} color="var(--gray-2)" />
-              <span className={styles.optionHeadingText}>Public</span>
+    const handleStatusChange = (ev: React.MouseEvent<HTMLLIElement>) => setStatus((ev.target as HTMLLIElement).dataset.value!)
+
+    return (
+        <details className={styles.boardStatus} open={open} ref={detailsRef}>
+            <summary className={styles.summary} onClick={handleClose}>
+                {isPrivate ?
+                    <Lock size={{width: 14, height: 14}} color="var(--gray-3)" />
+                    : <Globe size={{width: 14, height: 14}} color="var(--gray-2)" />
+                }
+                <span className={styles.summaryText}>{status}</span>
+            </summary>
+            <div className={styles.dropdown}>
+                <h4 className={styles.dropdownHeading}>Visibility</h4>
+                <p className={styles.dropdownDetails}>Choose who can view this board</p>
+                <ul className={styles.options}>
+                    <li className={styles.option} data-value="Public" onKeyDown={handleKeyDown} onClick={handleStatusChange} tabIndex={0}>
+                        <div className={styles.optionHeading}>
+                            <Globe size={{width: 14, height: 14}} color="var(--gray-2)" />
+                            <span className={styles.optionHeadingText}>Public</span>
+                        </div>
+                        <p className={styles.optionDetails}>Anyone on the internet can see this.</p>
+                    </li>
+                    <li onKeyDown={handleKeyDown} className={styles.option} data-value="Private" onClick={handleStatusChange} tabIndex={0}>
+                        <div className={styles.optionHeading}>
+                            <Lock size={{width: 14, height: 14}} color="var(--gray-2)" />
+                            <span className={styles.optionHeadingText}>Private</span>
+                        </div>
+                        <p className={styles.optionDetails}>Only board members can see this.</p>
+                    </li>
+                </ul>
             </div>
-            <p className={styles.optionDetails}>Anyone on the internet can see this.</p>
-          </li>
-          <li className={styles.option}>
-            <div className={styles.optionHeading}>
-              <Lock size={{ width: 14, height: 14 }} color="var(--gray-2)" />
-              <span className={styles.optionHeadingText}>Private</span>
-            </div>
-            <p className={styles.optionDetails}>Only board members can see this.</p>
-          </li>
-        </ul>
-      </div>
-    </details>
-  );
+        </details >
+    );
 };
 
-export default BoardStatus;
+export default React.memo(BoardStatus);
