91e333191b9f20e4281dfad9788177d5245a2996
jc <avaz28082000@gmail.com>
2020-11-12 23:55:52 +0500

    Add board route

---
+++ backend/src/routes/board.ts
@@ -0,0 +1,19 @@
+import { Router, Response, Request } from 'express';
+import { validationFor, validate } from '../utils/validator';
+import { verifyToken } from '../utils'
+import * as Board from '../services/board';
+
+const router = Router();
+
+router.post('/', verifyToken, async (req: Request, res: Response) => {
+    try {
+        const { _id } = req.user;
+        const { title, cover_photo_url, status } = req.body;
+        await Board.create({ title, visibility: status, cover_photo_url, creator: _id })
+        res.status(201).json({ message: 'Board created', statusCode: 201 })
+    } catch (err) {
+        res.status(err.statusCode).json({ message: err.message, statusCode: err.statusCode });
+    }
+})
+
+export default router;
