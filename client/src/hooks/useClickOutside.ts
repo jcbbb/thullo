28e0ebdc5bc8a1040696e126cc8d93b7ba83cd74
jc <avaz28082000@gmail.com>
2020-11-13 00:10:56 +0500

    move some logic into useEscape hook.

--- client/src/hooks/useClickOutside.ts
+++ client/src/hooks/useClickOutside.ts
@@ -1,25 +1,17 @@
-import {useRef, useEffect} from 'react';
+import { useRef, useEffect } from 'react';
 
 const useClickOutside = (elRef: React.RefObject<any>, callback: Function): void => {
     const callbackRef = useRef(callback);
     callbackRef.current = callback;
 
     useEffect(() => {
-        const handler = (ev: MouseEvent | KeyboardEvent) => {
-            if (!elRef?.current.contains(ev.target as Node)) {
+        const handler = (ev: MouseEvent) => {
+            if (!elRef?.current.contains(ev.target)) {
                 callbackRef.current(ev)
             }
-            if ('keyCode' in ev && ev.keyCode === 27) {
-                callbackRef.current(ev);
-            }
         }
-
         document.addEventListener('click', handler);
-        document.addEventListener('keyup', handler)
-        return () => {
-            document.removeEventListener('click', handler)
-            document.removeEventListener('keyup', handler)
-        }
+        return () => document.removeEventListener('click', handler)
     }, [elRef, callbackRef])
 }
 
