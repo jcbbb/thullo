6c5a93fc7da85d9cd6467204f5fc37dfdb1eda0f
jc <avaz28082000@gmail.com>
2020-11-13 00:02:54 +0500

    added cover_photo_url in type

--- backend/src/services/board.ts
+++ backend/src/services/board.ts
@@ -1,14 +1,15 @@
-import {Types} from 'mongoose'
-import {BadRequestError} from "../utils/errors"
+import { Types } from 'mongoose'
+import { BadRequestError } from "../utils/errors"
 import Board from '../models/board';
 
 type ICreate = {
     title: string;
     visibility: string;
-    creator: Types.ObjectId
+    creator: Types.ObjectId;
+    cover_photo_url: string;
 }
 
-export const create = async ({title, creator, visibility = 'public'}: ICreate) => {
+export const create = async ({ title, creator, visibility = 'Public', cover_photo_url }: ICreate) => {
     const isPublic: boolean = visibility === 'public';
     const isPrivate: boolean = !isPublic;
 
@@ -19,7 +20,7 @@ export const create = async ({title, creator, visibility = 'public'}: ICreate) =
         throw new BadRequestError("Creator's id is required")
     }
 
-    const board = new Board({title, creator, private: isPrivate, })
+    const board = new Board({ title, creator, private: isPrivate, cover_photo_url })
     await board.save();
 }
 
