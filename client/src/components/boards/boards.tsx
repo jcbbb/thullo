958b9e365f4fc48abd5775f8fced778e32006cb7
jc <avaz28082000@gmail.com>
2020-11-13 00:09:32 +0500

    include addBoard and open with backdrop

--- client/src/components/boards/boards.tsx
+++ client/src/components/boards/boards.tsx
@@ -1,21 +1,33 @@
 import * as React from 'react';
+import useBackdrop from '../../hooks/useBackdrop'
 import PlusIcon from '../icons/plus';
+import AddBoard from '../addBoard/addBoard';
+import Backdrop from '../backdrop/backdrop';
 import styles from './boards.module.scss';
 import buttonStyles from '../../styles/button.module.scss';
+import useEscape from '../../hooks/useEscape';
 
 const Boards = () => {
+    const { isActive, toggle } = useBackdrop()
+    useEscape(toggle)
+
     return (
-        <div className={styles.boards}>
-            <div className={styles.header}>
-                <h2 className={styles.heading}>All boards</h2>
-                <div>
-                    <button className={buttonStyles.primary}>
-                        <PlusIcon size={{width: 18, height: 18}} />
-                        <span className={styles.btnText}>Add</span>
-                    </button>
+        <>
+            <Backdrop isActive={isActive} close={toggle}>
+                <AddBoard />
+            </Backdrop>
+            <div className={styles.boards}>
+                <div className={styles.header}>
+                    <h2 className={styles.heading}>All boards</h2>
+                    <div>
+                        <button className={buttonStyles.primary} onClick={toggle}>
+                            <PlusIcon size={{ width: 18, height: 18 }} />
+                            <span className={styles.btnText}>Add</span>
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
