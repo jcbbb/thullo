65354ba045f80666dada7921c7ffc07d0d6f48ac
jc <avaz28082000@gmail.com>
2020-11-13 00:07:11 +0500

    remove addBoard from App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,8 @@ import Login from './components/login/login';
 import Signup from './components/signup/signup';
 import Center from './components/center/center';
 import Boards from './components/boards/boards';
-import AddBoard from './components/addBoard/addBoard'
-import {AuthProvider} from './context/authContext';
-import {Routes, Route} from 'react-router-dom';
+import { AuthProvider } from './context/authContext';
+import { Routes, Route } from 'react-router-dom';
 
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
         <div className="app">
             <AuthProvider>
                 <Header />
-                <AddBoard />
                 <Center>
                     <Boards />
                     <Routes>
