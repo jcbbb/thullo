57b614fe60db0bf3af3c5fa0178a43cc0cd1daf9
jc <avaz28082000@gmail.com>
2020-11-13 00:08:35 +0500

    verifyToken, permit function; remove refresh token

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -1,38 +1,25 @@
 import { Response, Request, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
 import { IUser } from '../models/user';
+import { AuthenticationError, AuthorizationError, BadRequestError } from './errors'
+import jwt from 'jsonwebtoken';
+import redisClient from '../services/redis';
 import config from '../config';
 
-interface Token {
-  accessToken: string;
-  refreshToken: string;
-}
+type AccessToken = string;
+type roles = 'user' | 'admin';
 
-export const generateToken = (user: IUser): Token => {
+export const generateToken = (user: IUser): AccessToken => {
   const { _id, email, role } = user;
   const accessToken = jwt.sign({ _id, email, role }, config.access_token_secret, {
     expiresIn: config.access_token_expiration,
   });
-  const refreshToken = jwt.sign({}, config.refresh_token_secret, {
-    expiresIn: `${config.refresh_token_expiration}d`,
-  });
-
-  return { accessToken, refreshToken };
+  return accessToken
 };
 
-export const setCookies = (res: Response, token: Token) => {
-  const day = 24 * 3600000;
-  const refreshTokenExpiration = parseInt(config.refresh_token_expiration, 10);
+export const setCookies = (res: Response, accessToken: AccessToken) => {
   const accessTokenExpiration = parseInt(config.access_token_expiration, 10);
-  const { accessToken, refreshToken } = token;
 
   return res
-    .cookie('refresh_token', refreshToken, {
-      expires: new Date(Date.now() + refreshTokenExpiration * day),
-      secure: true,
-      sameSite: true,
-      httpOnly: true,
-    })
     .cookie('access_token', accessToken, {
       expires: new Date(Date.now() + accessTokenExpiration),
       secure: true,
@@ -43,4 +30,35 @@ export const setCookies = (res: Response, token: Token) => {
 
 export const randomNumber = () => Math.floor(100000 + Math.random() * 900000);
 
-export const verifyToken = (res: Response, req: Request, next: NextFunction) => {};
+export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+  const accessToken = req.cookies.access_token || '';
+  if (!accessToken) {
+    throw new BadRequestError('Access token is required')
+  }
+
+  jwt.verify(accessToken, (err, decoded) => {
+    if (!err) {
+      redisClient.get((decoded as any)._id, (err, reply) => {
+        if (err) {
+          throw new Error('Something went wrong with redis')
+        }
+        if (reply) {
+          throw new AuthenticationError('This user has been blocked by admin')
+        }
+      })
+      req.user = decoded as any;
+      next()
+    }
+  })
+};
+
+export const permit = (...permittedRoles: roles[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user;
+    if (user && permittedRoles.includes(user.role as roles)) {
+      return next()
+    } else {
+      throw new AuthorizationError('Forbidden');
+    }
+  }
+}
