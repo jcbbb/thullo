df018a7cb1008c9bc9c3eeda7ecf3867e22a12ee
jc <avaz28082000@gmail.com>
2020-11-13 23:24:55 +0500

    add boards route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import auth from './auth';
+import board from './board'
 
 const route = Router();
 
 route.use('/auth', auth);
+route.use('/boards', board)
 
 export default route;
