005f63c13ac5bc78e95add7c6ec799f2ddba7fe3
jc <avaz28082000@gmail.com>
2020-11-13 23:25:52 +0500

    pass params: err, req, res, next

--- backend/src/utils/error-handler.ts
+++ backend/src/utils/error-handler.ts
@@ -1,6 +1,6 @@
-import { Response } from 'express'
+import { Response, Request, NextFunction } from 'express'
 import { IError } from './errors'
 
-export const errorHandler = (err: IError, res: Response) => {
-    return res.status(err.statusCode!).json({ ...err })
+export const errorHandler = (err: IError, req: Request, res: Response, next: NextFunction) => {
+    res.status(err.statusCode!).json({ ...err })
 }
