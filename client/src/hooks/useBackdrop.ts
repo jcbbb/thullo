d2324a5944fe20aac5a57c57aa00e235bdd9fb97
jc <avaz28082000@gmail.com>
2020-11-15 21:28:41 +0500

    add dismiss function

--- client/src/hooks/useBackdrop.ts
+++ client/src/hooks/useBackdrop.ts
@@ -4,13 +4,15 @@ import { useState } from 'react';
 type IBackdropHook = {
     isActive: boolean;
     toggle: () => void;
+    dismiss: () => void;
 }
 
 const useBackdrop = (): IBackdropHook => {
     const [isActive, setIsActive] = useState(false);
     const toggle = () => setIsActive((prev) => !prev);
+    const dismiss = () => setIsActive(false)
 
-    return { isActive, toggle }
+    return { isActive, toggle, dismiss }
 }
 
 export default useBackdrop;
