672600de57ddfa412681b2f6bce56fb584fdd4ee
jc <avaz28082000@gmail.com>
2020-11-17 16:23:19 +0500

    Restructure project for Auth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,21 @@
 import * as React from 'react';
 import Header from './components/header/header';
-import Login from './components/login/login';
-import Signup from './components/signup/signup';
 import Center from './components/center/center';
-import Boards from './components/boards/boards';
+import AuthChecker from './components/authChecker';
+import RenderedRoutes from './components/routes'
 import { AuthProvider } from './context/authContext';
-import { Routes, Route } from 'react-router-dom';
 
 
 function App() {
     return (
         <div className="app">
             <AuthProvider>
-                <Header />
-                <Center>
-                    <Boards />
-                    <Routes>
-                        <Route path="/">
-                            <Signup />
-                        </Route>
-                        <Route path="/login">
-                            <Login />
-                        </Route>
-                    </Routes>
-                </Center>
+                <AuthChecker>
+                    <Header />
+                    <Center>
+                        <RenderedRoutes />
+                    </Center>
+                </AuthChecker>
             </AuthProvider>
         </div>
     );
