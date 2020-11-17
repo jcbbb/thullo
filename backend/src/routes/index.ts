51f2fcd09921bc26662a4c0d9661bfd3493399bf
jc <avaz28082000@gmail.com>
2020-11-17 18:49:05 +0500

    include user route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import auth from './auth';
 import board from './board'
+import user from './user';
 
 const route = Router();
 
 route.use('/auth', auth);
 route.use('/boards', board)
+route.use('users', user)
 
 export default route;
