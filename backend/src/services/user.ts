6105a442405b580c03ef8d58b512fde9546bce91
jc <avaz28082000@gmail.com>
2020-11-17 18:50:35 +0500

    user service functions

---
+++ backend/src/services/user.ts
@@ -0,0 +1,11 @@
+import { Types } from 'mongoose';
+import { BadRequestError } from '../utils/errors'
+import User, { IUser } from '../models/user';
+
+export const getUserById = async (id: Types.ObjectId): Promise<IUser> => {
+    const user = await User.findById(id);
+    if (!user) {
+        throw new BadRequestError(`User with id of ${id} does not exist`)
+    }
+    return user;
+}
