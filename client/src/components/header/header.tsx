f2821a3252b44ad4a1f4eef486033964fa9ed368
jc <avaz28082000@gmail.com>
2020-11-17 16:27:57 +0500

    Restructure to hidden component

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,36 +1,37 @@
 import * as React from 'react';
-import {useState} from 'react';
-import {Link} from 'react-router-dom';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import ProfilePic from '../profilePic/profilePic';
+import Hidden from '../hidden/hidden'
 import LogoIcon from '../icons/thullo';
 import ChevronUpIcon from '../icons/chevron-up';
 import ChevronDownIcon from '../icons/chevron-down';
 import ApplicationIcon from '../icons/application';
-import {useAuthStore} from '../../context/authContext'
 import styles from './header.module.scss';
 
 const Header = () => {
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState('');
-    const {isAuthenticated} = useAuthStore();
     return (
         <div className={styles.header}>
             <div className={styles.headerLeft}>
                 <Link to="/">
-                    <LogoIcon viewBox="0 0 98 29" size={{width: 96, height: 54}} />
+                    <LogoIcon viewBox="0 0 98 29" size={{ width: 96, height: 54 }} />
                 </Link>
-                <div className={styles.headerLeftInner}>
-                    <div className={styles.boardName}>
-                        <h2 className={styles.boardNameText}>Devchallenges Board</h2>
+                <Hidden>
+                    <div className={styles.headerLeftInner}>
+                        <div className={styles.boardName}>
+                            <h2 className={styles.boardNameText}>Devchallenges Board</h2>
+                        </div>
+                        <span className={styles.seperator}></span>
+                        <Link className={styles.boards} to="/">
+                            <ApplicationIcon size={{ width: 14, height: 14 }} color="var(--gray-3)" />
+                            <span className={styles.boardsText}>All board</span>
+                        </Link>
                     </div>
-                    <span className={styles.seperator}></span>
-                    <Link className={styles.boards} to="/">
-                        <ApplicationIcon size={{width: 14, height: 14}} color="var(--gray-3)" />
-                        <span className={styles.boardsText}>All board</span>
-                    </Link>
-                </div>
+                </Hidden>
             </div>
-            {isAuthenticated ? (
+            <Hidden>
                 <div className={styles.headerRight}>
                     <form className={styles.form}>
                         <input
@@ -41,9 +42,9 @@ const Header = () => {
                             value={value}
                             onChange={(ev) => setValue(ev.target.value)}
                         />
-                        <button className={styles.formBtn} style={{width: 'fit-content'}} disabled={!value}>
+                        <button className={styles.formBtn} style={{ width: 'fit-content' }} disabled={!value}>
                             Search
-          </button>
+                        </button>
                     </form>
                     <div className={styles.profile}>
                         <ProfilePic />
@@ -60,19 +61,19 @@ const Header = () => {
                                 <li className={styles.linkItem}>
                                     <Link to="/settings" className={styles.link}>
                                         Settings
-                </Link>
+                                    </Link>
                                 </li>
                                 <li className={styles.linkItem}>
                                     <Link to="/logout" className={styles.link}>
                                         Log out
-                </Link>
+                                    </Link>
                                 </li>
                             </ul>
                         </details>
                     </div>
                 </div>
-
-            ) : null} </div>
+            </Hidden>
+        </div>
     );
 };
 
