f270d97accda2737000e9a2a6b29bfee44763e24
jcbbb <avaz28082000@gmail.com>
2021-01-08 17:29:25 +0500

    added more user routes

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
-import { verifyToken } from '../utils';
+import { verifyToken, validate } from '../utils';
 import * as UserController from '../controllers/user.controller';
 
 const router = Router();
 
-router.get('/profile', verifyToken, UserController.profile);
+router.get('/profile', verifyToken, UserController.getProfile);
+router.get('/', validate('limit', 'offset'), UserController.getAllUsers);
+router.get('/:userId', UserController.getUserById);
+router.delete('/:userId', verifyToken, UserController.deleteUserById);
+router.patch('/:userId', UserController.patchUserById);
 
 export default router;
