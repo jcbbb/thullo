2018ff105d19a64c917bb026a822a1bcc5a2ac2f
jcbbb <avaz28082000@gmail.com>
2021-01-08 17:27:42 +0500

    add morgan; handle all exceptions

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,15 @@ import config from './config';
 import router from './routes';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 import { errorHandler } from './utils/error-handler';
 import * as db from './services/db.service';
+import { logger } from './utils/logger';
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('combined'));
 app.use(cookieParser());
 app.use(
   cors({
@@ -21,5 +24,14 @@ app.use('/', router);
 app.use(errorHandler);
 
 db.connect().then(() => {
-  app.listen(config.port, () => console.log(`Server started on port ${config.port}`));
+  app.listen(config.port, () => logger.info(`Server started on port ${config.port}`));
 });
+
+process
+  .on('uncaughtException', (err) => {
+    logger.error('uncaughtException happened. Exiting the process', err);
+    process.exit(1);
+  })
+  .on('unhandledRejection', (reason, p) => {
+    logger.error(reason!, `unhandledRejection at ${p}`);
+  });
