420a77097be1992edf71aa47594782ef133ed51a
jcbbb <avaz28082000@gmail.com>
2021-01-08 17:32:28 +0500

    authChecker component; WIP

--- client/src/components/authChecker.tsx
+++ client/src/components/authChecker.tsx
@@ -1,30 +1,30 @@
 import * as React from 'react';
 import { useCallback } from 'react';
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 import { useAuthDispatch } from '../context/authContext';
 import useAsync from '../hooks/useAsync';
-import api from '../api'
+import api from '../api';
 
 const AuthChecker: React.FC = ({ children }) => {
-    const authDispatch = useAuthDispatch()
-    const [verifyToken, checkAuthState] = useAsync(api.auth.checkAuth);
+  const authDispatch = useAuthDispatch();
+  const [verifyToken, checkAuthState] = useAsync(api.auth.checkAuth);
 
-    const checkAuth = useCallback(() => {
-        verifyToken()
-        authDispatch({ type: 'LOGIN' })
-    }, [verifyToken, authDispatch]);
+  const checkAuth = useCallback(async () => {
+    await verifyToken();
+    authDispatch({ type: 'LOGIN' });
+  }, [verifyToken, authDispatch]);
 
-    useEffect(() => {
-        checkAuth()
-    }, [checkAuth])
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
 
-    useEffect(() => {
-        if (checkAuthState.isError) {
-            authDispatch({ type: 'LOGOUT' })
-        }
-    }, [checkAuthState.isError, authDispatch])
+  useEffect(() => {
+    if (checkAuthState.isError) {
+      authDispatch({ type: 'LOGOUT' });
+    }
+  }, [checkAuthState.isError, authDispatch]);
 
-    return <>{children}</>
-}
+  return <>{children}</>;
+};
 
 export default AuthChecker;
