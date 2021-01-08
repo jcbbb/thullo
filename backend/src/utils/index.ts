88ab8274048d8dfa1673d7f58b84f401c75586f2
jcbbb <avaz28082000@gmail.com>
2021-01-08 17:26:04 +0500

    use index for exporting

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -1,68 +1,7 @@
-import { Response, Request, NextFunction } from 'express';
-import { IUser } from '../models/user.model';
-import { AuthenticationError, AuthorizationError, BadRequestError } from './errors'
-import jwt from 'jsonwebtoken';
-import redisClient from '../services/redis.service';
-import config from '../config';
-
-type AccessToken = string;
-type roles = 'user' | 'admin';
-
-export const generateToken = (user: IUser): AccessToken => {
-  const { _id, email, role } = user;
-  const accessToken = jwt.sign({ _id, email, role }, config.access_token_secret, {
-    expiresIn: config.access_token_expiration,
-  });
-  return accessToken
-};
-
-export const setCookies = (res: Response, accessToken: AccessToken) => {
-  const accessTokenExpiration = parseInt(config.access_token_expiration, 10);
-
-  return res
-    .cookie('access_token', accessToken, {
-      expires: new Date(Date.now() + accessTokenExpiration),
-      secure: true,
-      sameSite: true,
-      httpOnly: true,
-    });
-};
-
 export const randomNumber = () => Math.floor(100000 + Math.random() * 900000);
-
-export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.get('Authorization')?.split(" ");
-  const fromHeader = authHeader && authHeader[0] === 'Bearer' ? authHeader[1] : '';
-  const accessToken = req.cookies.access_token || fromHeader || '';
-
-  if (!accessToken) {
-    return next(new BadRequestError('Access token is required'))
-  }
-
-  jwt.verify(accessToken, config.access_token_secret, (err: any, decoded: any) => {
-    if (!err) {
-      redisClient.get(decoded._id, (err, reply) => {
-        if (err) {
-          return next(new Error('Something went wrong with redis'))
-        }
-        if (reply) {
-          return next(new AuthenticationError('This user has been blocked by admin'))
-        }
-      });
-
-      req.user = decoded;
-      next()
-    }
-  })
-};
-
-export const permit = (...permittedRoles: roles[]) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const user = req.user;
-    if (user && permittedRoles.includes(user.role as roles)) {
-      return next()
-    } else {
-      throw new AuthorizationError('Forbidden');
-    }
-  }
-}
+export { permit } from './permit';
+export { asyncHandler } from './async-handler';
+export { logger } from './logger';
+export { validate } from './validator';
+export { verifyToken } from './verify-token';
+export { generateToken } from './generate-token';
