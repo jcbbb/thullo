9ba2d9c5c76b9214e880d46932d13ea171ee71f1
jcbbb <avaz28082000@gmail.com>
2021-01-08 17:31:11 +0500

    generate token function

---
+++ backend/src/utils/generate-token.ts
@@ -0,0 +1,12 @@
+import { IUser } from '../interfaces/user.interface';
+import jwt from 'jsonwebtoken';
+import config from '../config';
+
+type AccessToken = 'string';
+export const generateToken = (user: IUser): AccessToken => {
+  const { _id, email, role } = user;
+  const accessToken = jwt.sign({ _id, email, role }, config.access_token_secret, {
+    expiresIn: config.access_token_expiration,
+  });
+  return accessToken as AccessToken;
+};
