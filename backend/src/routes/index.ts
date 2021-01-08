602e3c6b3187df081f034523796d7fb7bba6fe60
jcbbb <avaz28082000@gmail.com>
2021-01-08 17:29:08 +0500

    add board route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import auth from './auth.route';
-import board from './board.route'
+import board from './board.route';
 import user from './user.route';
 
 const route = Router();
 
 route.use('/auth', auth);
-route.use('/boards', board)
-route.use('users', user)
+route.use('/boards', board);
+route.use('/users', user);
 
 export default route;
