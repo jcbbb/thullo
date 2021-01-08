6ade55210dcc9fd840fa0e2eaf0eda4f9bbb1ccc
jcbbb <avaz28082000@gmail.com>
2021-01-08 17:28:09 +0500

    added more controllers for user

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,9 +1,41 @@
-import { Response, Request, NextFunction } from 'express';
+import { Response, Request } from 'express';
 import { asyncHandler } from '../utils/async-handler';
+import { IUser, IUserFilterOpts } from '../interfaces/user.interface';
 import * as UserService from '../services/user.service';
 
-export const profile = asyncHandler(async (req: Request, res: Response) => {
+export const getProfile = asyncHandler(async (req: Request, res: Response) => {
   const { _id } = req.user;
   const user = await UserService.getUserById(_id);
-  res.status(200).json({ user });
+  res.status(200).json(user);
+});
+
+export const getAllUsers = asyncHandler(async (req: Request, res: Response) => {
+  let { limit, offset } = req.query;
+  const users = await UserService.getAllUsers({ limit, offset } as IUserFilterOpts);
+  res.status(200).json(users);
+});
+
+export const getUserById = asyncHandler(async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  const user = await UserService.getUserById(userId);
+  res.status(200).json(user);
+});
+
+export const deleteUserById = asyncHandler(async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  const deletedUser = await UserService.deleteUserById(userId);
+  res.status(200).json(deletedUser);
+});
+
+export const patchUserById = asyncHandler(async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  const { email, password, name, profile_photo_url } = req.body;
+
+  const patchedUser = await UserService.patchUserById(userId, {
+    email,
+    password,
+    name,
+    profile_photo_url,
+  } as IUser);
+  res.status(200).json(patchedUser);
 });
