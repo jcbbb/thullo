2e22cecf3bd619e5127e3fca5ebc524b5b54d0b4
jcbbb <avaz28082000@gmail.com>
2021-01-08 17:30:22 +0500

    more user methods

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,11 +1,56 @@
-import { Types } from 'mongoose';
+import { isValidObjectId } from 'mongoose';
 import { BadRequestError } from '../utils/errors';
-import User, { IUser } from '../models/user.model';
+import User from '../models/user.model';
+import { IUser, IUserFilterOpts } from '../interfaces/user.interface';
 
-export const getUserById = async (id: Types.ObjectId): Promise<IUser> => {
-  const user = await User.findById(id, { password: 0 });
+const defaultProjection = {
+  passowrd: 0,
+  __v: 0,
+};
+
+export const getUserById = async (id: string): Promise<IUser> => {
+  if (!isValidObjectId(id)) {
+    throw new BadRequestError('Invalid user id');
+  }
+  const user = await User.findById(id, defaultProjection);
   if (!user) {
     throw new BadRequestError(`User with id of ${id} does not exist`);
   }
   return user;
+};
+
+export const deleteUserById = async (id: string) => {
+  if (!isValidObjectId(id)) {
+    throw new BadRequestError('Invalid user id');
+  }
+  const deletedUser = await User.findByIdAndRemove(id, { projection: defaultProjection });
+  if (!deletedUser) {
+    throw new BadRequestError(`User with id of ${id} does not exist`);
+  }
+
+  return deletedUser;
+};
+export const getAllUsers = async ({
+  limit = 10,
+  offset = 0,
+}: IUserFilterOpts): Promise<IUser[]> => {
+  return await User.find({}, defaultProjection)
+    /* 
+      `+offset` converts string type to number type. If can't be converted, 
+      NaN will be returned and .skip will get 0 as value. Same logic goes for .limit
+    */
+    .skip(+offset || 0)
+    .limit(Math.min(+limit || 10, 100));
+};
+
+export const patchUserById = async (id: string, update: IUser) => {
+  if (!isValidObjectId(id)) {
+    throw new BadRequestError('Invalid user id');
+  }
+
+  return await User.findOneAndUpdate({ _id: id }, update, {
+    projection: defaultProjection,
+    new: true,
+    runValidators: true,
+  });
 };
