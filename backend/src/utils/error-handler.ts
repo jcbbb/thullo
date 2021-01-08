e23bc572b421e4afd49d0781a98a1b7e00fe4f74
jcbbb <avaz28082000@gmail.com>
2021-01-08 17:30:36 +0500

    improved error handling

--- backend/src/utils/error-handler.ts
+++ backend/src/utils/error-handler.ts
@@ -1,6 +1,15 @@
-import { Response, Request, NextFunction } from 'express'
-import { IError } from './errors'
+import { Response, Request, NextFunction } from 'express';
+import { Error } from 'mongoose';
+import { IError, ValidationError } from './errors';
 
 export const errorHandler = (err: IError, req: Request, res: Response, next: NextFunction) => {
-    res.status(err.statusCode!).json({ ...err })
-}
+  if (err instanceof Error.ValidationError) {
+    const normalizedErrors = {} as any;
+    for (const error in err.errors) {
+      normalizedErrors[error] = err.errors[error].message;
+    }
+    const newError = new ValidationError('Validation failed', 422, normalizedErrors);
+    return res.status(newError.statusCode).json({ ...newError });
+  }
+  res.status(err.statusCode || 500).json({ ...err });
+};
