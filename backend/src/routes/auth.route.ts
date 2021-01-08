74d0639e53614af791aa4c677f7f2f1ab691413d
jcbbb <avaz28082000@gmail.com>
2021-01-08 17:28:52 +0500

    import better

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
-import { validate } from '../utils/validator';
-import { verifyToken } from '../utils';
+import { verifyToken, validate } from '../utils';
 import * as AuthController from '../controllers/auth.controller';
 
 const router = Router();
@@ -8,7 +7,7 @@ const router = Router();
 router.post('/check-email', validate('email'), AuthController.checkEmail);
 router.post('/temp-user', validate('email'), AuthController.tempUser);
 router.post('/verify', validate('email', 'authCode'), AuthController.verify);
-router.post('/signup', validate('email', 'password', 'authCode'), AuthController.signup);
+router.post('/signup', validate('email', 'password', 'authCode', 'name'), AuthController.signup);
 router.post('/login', validate('email', 'password'), AuthController.login);
 
 router.get('/me', verifyToken, AuthController.me);
